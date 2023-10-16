a7fe7f462fc0ec74761d9994981b805ebace7d5c
lse415 <leesuel0379@gmail.com>
2023-10-17 02:56:07 +0900

    computed value

--- udemy_react/src/components/Expenses/Expenses.js
+++ udemy_react/src/components/Expenses/Expenses.js
@@ -1,12 +1,35 @@
 import React from "react";
-
+import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  //computed value. value which derived based on the state
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filteredYear === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filteredYear === "2021") {
+    filterInfoText = "2019, 2020 & 2022";
+  } else {
+    filterInfoText = "2019, 2020 & 2021";
+  }
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {/* feature to study 'computed state' */}
+      <p>Data for years {filterInfoText} is hidden.</p>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
