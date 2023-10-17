6490fbeb1eb44627b68141a6da1bcb554c035dd6
lse415 <leesuel0379@gmail.com>
2023-10-17 10:30:19 +0900

    86. rendering lists of data

--- udemy_react/src/components/Expenses/Expenses.js
+++ udemy_react/src/components/Expenses/Expenses.js
@@ -30,26 +30,14 @@ const Expenses = (props) => {
       />
       {/* feature to study 'computed state' */}
       <p>Data for years {filterInfoText} is hidden.</p>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
